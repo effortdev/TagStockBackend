5b9c3db6f7874de0f597cb40f54af927f16b2927
ktj <kindxowns@gmail.com>
2026-05-24 16:22:53 +0900

    TagStockBackend 기본세팅

---
+++ src/main/java/com/tagstock/tagstockbackend/TagStockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tagstock.tagstockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagStockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagStockBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tagstock/tagstockbackend/TagStockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagstock.tagstockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagStockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
